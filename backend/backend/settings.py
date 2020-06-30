1f368991fd013fa3fdab2eb78dc97e7add0dcb94
Arjun SK <dev.arjunsk@gmail.com>
2020-06-30 13:03:04 +0530

    Initial Api setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from ...secrets import secretkey
+from .secrets import secretkey
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
